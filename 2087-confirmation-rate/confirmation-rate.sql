0b2976b143dd32bc1ee9d1c845cbd354dc79c930
Priyesh Gupta <68697132+Kalinga07@users.noreply.github.com>
2024-05-23 23:57:16 +0530

    Time: 1469 ms (47.65%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
-SELECT              s.user_id,
-ROUND(CAST(COUNT(CASE WHEN action = 'confirmed' THEN 1 ELSE NULL END) AS FLOAT) / COUNT(*), 2) as confirmation_rate
-FROM                Signups s
-LEFT JOIN     Confirmations c
-ON                  s.user_id = c.user_id
-GROUP BY            s.user_id
+/* Write your PL/SQL query statement below */
+/* Write your PL/SQL query statement below */
+
+select s.user_id ,round(avg(case when c.action='confirmed' then 1 else 0 end),2) confirmation_rate
+from signups s left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
